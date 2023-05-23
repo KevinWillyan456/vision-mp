8bb889eed8131a004d23c679321e4a46dea7766a
Kevin Souza <kevinwillyansouza@gmail.com>
2023-05-23 17:01:07 -0400

    Ponto de Controle

--- js/index.js
+++ js/index.js
@@ -39,185 +39,88 @@ const data = [
     },
     {
         id: 5,
-        image: 'https://drive.google.com/uc?export=download&id=1e4J6KT6xVrWqyim_MhavSOzBKDOck1Vo',
+        image: 'https://drive.google.com/uc?export=download&id=1FH3tOcV0dFOND4_wS0gKDZyq2j-jcgg_',
     },
     {
         id: 6,
-        image: 'https://drive.google.com/uc?export=download&id=1VR4WJtXcran8w3sl4E_wMtu5o3RgNssD',
+        image: 'https://drive.google.com/uc?export=download&id=1PEan_f54sPk0aBSZsmc70aVEZVrFjjJY',
     },
     {
         id: 7,
-        image: 'https://drive.google.com/uc?export=download&id=1lLjjpIjmLUJCWKLzuiwNo8bPumnOYIp5',
+        image: 'https://drive.google.com/uc?export=download&id=1KFJ4aqAQMyX0slOgRDKY_AbmIdFrspkX',
     },
     {
         id: 8,
-        image: 'https://drive.google.com/uc?export=download&id=1OWiMJUNsM1Ig6wfwdmhiY_fOq7uvUqjD',
+        image: 'https://drive.google.com/uc?export=download&id=1fuMbBEl34z9uy7w_etxeINI9NlDrRqI_',
     },
     {
         id: 9,
-        image: 'https://drive.google.com/uc?export=download&id=1e4J6KT6xVrWqyim_MhavSOzBKDOck1Vo',
+        image: 'https://drive.google.com/uc?export=download&id=1FAU4LkB7N0hVcc3dsPAiF7wTGwXLDDRV',
     },
     {
         id: 10,
-        image: 'https://drive.google.com/uc?export=download&id=1VR4WJtXcran8w3sl4E_wMtu5o3RgNssD',
+        image: 'https://drive.google.com/uc?export=download&id=1gsDDitY9-lNiL7diHpuMceC7uPH_Hrwe',
     },
     {
         id: 11,
-        image: 'https://drive.google.com/uc?export=download&id=1lLjjpIjmLUJCWKLzuiwNo8bPumnOYIp5',
+        image: 'https://drive.google.com/uc?export=download&id=1UmZsMh5bF9qkTjRO7HjYQ6FQ01ITj2mZ',
     },
     {
         id: 12,
-        image: 'https://drive.google.com/uc?export=download&id=1OWiMJUNsM1Ig6wfwdmhiY_fOq7uvUqjD',
+        image: 'https://drive.google.com/uc?export=download&id=1WD2L2vaP7W9FTw2ezRHDP9lA-cc0Zh70',
     },
     {
         id: 13,
-        image: 'https://drive.google.com/uc?export=download&id=1e4J6KT6xVrWqyim_MhavSOzBKDOck1Vo',
+        image: 'https://drive.google.com/uc?export=download&id=1WLI7UXhvZXsztyf2EWVvhMQ0TWiTtTm5',
     },
     {
         id: 14,
-        image: 'https://drive.google.com/uc?export=download&id=1VR4WJtXcran8w3sl4E_wMtu5o3RgNssD',
+        image: 'https://drive.google.com/uc?export=download&id=1YK75zRLTbTwkHNeUR06pUlrNSZGd1Fpl',
     },
     {
         id: 15,
-        image: 'https://drive.google.com/uc?export=download&id=1lLjjpIjmLUJCWKLzuiwNo8bPumnOYIp5',
+        image: 'https://drive.google.com/uc?export=download&id=1cedm_IUS7QM6aSUHqGmA9mtrvITUAGDf',
     },
     {
         id: 16,
-        image: 'https://drive.google.com/uc?export=download&id=1OWiMJUNsM1Ig6wfwdmhiY_fOq7uvUqjD',
+        image: 'https://drive.google.com/uc?export=download&id=1pGwQhRRShjXUqMbBMI2sWHyYMzUsfBnh',
     },
     {
         id: 17,
-        image: 'https://drive.google.com/uc?export=download&id=1e4J6KT6xVrWqyim_MhavSOzBKDOck1Vo',
+        image: 'https://drive.google.com/uc?export=download&id=10qMaRJ0Rs1g_RhIWNKdAjPU97LtdoiCh',
     },
     {
         id: 18,
-        image: 'https://drive.google.com/uc?export=download&id=1VR4WJtXcran8w3sl4E_wMtu5o3RgNssD',
+        image: 'https://drive.google.com/uc?export=download&id=1TUS3zPUa_RpkpeZ6YIR2NEJYDmvOzoB8',
     },
     {
         id: 19,
-        image: 'https://drive.google.com/uc?export=download&id=1lLjjpIjmLUJCWKLzuiwNo8bPumnOYIp5',
+        image: 'https://drive.google.com/uc?export=download&id=1keZvoUy5w84CTUyD9G89Lkz82_8qwkuS',
     },
     {
         id: 20,
-        image: 'https://drive.google.com/uc?export=download&id=1OWiMJUNsM1Ig6wfwdmhiY_fOq7uvUqjD',
+        image: 'https://drive.google.com/uc?export=download&id=1c5c6E3tUeI8Lq4glRKWbKi59iD6ZxS1C',
     },
     {
         id: 21,
-        image: 'https://drive.google.com/uc?export=download&id=1e4J6KT6xVrWqyim_MhavSOzBKDOck1Vo',
+        image: 'https://drive.google.com/uc?export=download&id=1fnas4xygZRB2rgDDmCPR-pM2-ljTgpn5',
     },
     {
         id: 22,
-        image: 'https://drive.google.com/uc?export=download&id=1VR4WJtXcran8w3sl4E_wMtu5o3RgNssD',
+        image: 'https://drive.google.com/uc?export=download&id=18M7dCd6jkqVPFKlFZVUprBafsWHIYuXe',
     },
     {
         id: 23,
-        image: 'https://drive.google.com/uc?export=download&id=1lLjjpIjmLUJCWKLzuiwNo8bPumnOYIp5',
+        image: 'https://drive.google.com/uc?export=download&id=1KFT-xw0lZ6sOe_-ZGoHdM8SmWg0SdaqF',
     },
     {
         id: 24,
-        image: 'https://drive.google.com/uc?export=download&id=1OWiMJUNsM1Ig6wfwdmhiY_fOq7uvUqjD',
-    },
-    {
-        id: 25,
-        image: 'https://drive.google.com/uc?export=download&id=1e4J6KT6xVrWqyim_MhavSOzBKDOck1Vo',
-    },
-    {
-        id: 26,
-        image: 'https://drive.google.com/uc?export=download&id=1VR4WJtXcran8w3sl4E_wMtu5o3RgNssD',
-    },
-    {
-        id: 27,
-        image: 'https://drive.google.com/uc?export=download&id=1lLjjpIjmLUJCWKLzuiwNo8bPumnOYIp5',
-    },
-    {
-        id: 28,
-        image: 'https://drive.google.com/uc?export=download&id=1OWiMJUNsM1Ig6wfwdmhiY_fOq7uvUqjD',
-    },
-    {
-        id: 29,
-        image: 'https://drive.google.com/uc?export=download&id=1e4J6KT6xVrWqyim_MhavSOzBKDOck1Vo',
-    },
-    {
-        id: 30,
-        image: 'https://drive.google.com/uc?export=download&id=1VR4WJtXcran8w3sl4E_wMtu5o3RgNssD',
-    },
-    {
-        id: 31,
-        image: 'https://drive.google.com/uc?export=download&id=1lLjjpIjmLUJCWKLzuiwNo8bPumnOYIp5',
-    },
-    {
-        id: 32,
-        image: 'https://drive.google.com/uc?export=download&id=1OWiMJUNsM1Ig6wfwdmhiY_fOq7uvUqjD',
-    },
-    {
-        id: 33,
-        image: 'https://drive.google.com/uc?export=download&id=1e4J6KT6xVrWqyim_MhavSOzBKDOck1Vo',
-    },
-    {
-        id: 34,
-        image: 'https://drive.google.com/uc?export=download&id=1VR4WJtXcran8w3sl4E_wMtu5o3RgNssD',
-    },
-    {
-        id: 35,
-        image: 'https://drive.google.com/uc?export=download&id=1lLjjpIjmLUJCWKLzuiwNo8bPumnOYIp5',
-    },
-    {
-        id: 36,
-        image: 'https://drive.google.com/uc?export=download&id=1OWiMJUNsM1Ig6wfwdmhiY_fOq7uvUqjD',
-    },
-    {
-        id: 37,
-        image: 'https://drive.google.com/uc?export=download&id=1e4J6KT6xVrWqyim_MhavSOzBKDOck1Vo',
-    },
-    {
-        id: 38,
-        image: 'https://drive.google.com/uc?export=download&id=1VR4WJtXcran8w3sl4E_wMtu5o3RgNssD',
-    },
-    {
-        id: 39,
-        image: 'https://drive.google.com/uc?export=download&id=1lLjjpIjmLUJCWKLzuiwNo8bPumnOYIp5',
-    },
-    {
-        id: 40,
-        image: 'https://drive.google.com/uc?export=download&id=1OWiMJUNsM1Ig6wfwdmhiY_fOq7uvUqjD',
-    },
-    {
-        id: 41,
-        image: 'https://drive.google.com/uc?export=download&id=1e4J6KT6xVrWqyim_MhavSOzBKDOck1Vo',
-    },
-    {
-        id: 42,
-        image: 'https://drive.google.com/uc?export=download&id=1VR4WJtXcran8w3sl4E_wMtu5o3RgNssD',
-    },
-    {
-        id: 43,
-        image: 'https://drive.google.com/uc?export=download&id=1lLjjpIjmLUJCWKLzuiwNo8bPumnOYIp5',
-    },
-    {
-        id: 44,
-        image: 'https://drive.google.com/uc?export=download&id=1OWiMJUNsM1Ig6wfwdmhiY_fOq7uvUqjD',
-    },
-    {
-        id: 45,
-        image: 'https://drive.google.com/uc?export=download&id=1e4J6KT6xVrWqyim_MhavSOzBKDOck1Vo',
-    },
-    {
-        id: 46,
-        image: 'https://drive.google.com/uc?export=download&id=1VR4WJtXcran8w3sl4E_wMtu5o3RgNssD',
-    },
-    {
-        id: 47,
-        image: 'https://drive.google.com/uc?export=download&id=1lLjjpIjmLUJCWKLzuiwNo8bPumnOYIp5',
-    },
-    {
-        id: 48,
-        image: 'https://drive.google.com/uc?export=download&id=1OWiMJUNsM1Ig6wfwdmhiY_fOq7uvUqjD',
+        image: 'https://drive.google.com/uc?export=download&id=1ISGpCLl5V2kTQ2IP7hsTrZiYsT3xECSE',
     },
 ]
 
-// let newArray = data.slice(0, 5);
 let preparedArray = []
-let shuffledData
+let shuffledData = []
 
 const gameConfig = {
     boardSize: {
@@ -247,16 +150,17 @@ function actionsInTheElement() {
     const allPieces = document.querySelectorAll('.piece')
 
     allPieces.forEach((element) => {
-        // console.log(allPieces)
         element.addEventListener('click', () => {
             if (running) {
-                if ($(element).hasClass('turn')) {
+                if (
+                    $(element).hasClass('turn') ||
+                    $(element).hasClass('turned')
+                ) {
                     return
                 } else {
                     manageIinteractionsWithBoard($(element).data('id'))
                     element.classList.toggle('turn')
                 }
-                // alert($(element).data('id'))
             }
         })
     })
@@ -350,6 +254,11 @@ function startButtonFuction() {
         stageElementsStart.forEach((element) => $(element).toggle())
         stageElementsBoardChoice.forEach((element) => $(element).toggle())
     })
+    document
+        .querySelector(
+            '.main-game .completed-game .play-again .icon-play-again'
+        )
+        .addEventListener('click', routinePlayAgain)
 }
 
 function manageIinteractionsWithBoard(value) {
@@ -382,22 +291,36 @@ function endGame() {
     switch (indexBoardChoice) {
         case 'small':
             if (allTurned.length >= 6) {
-                alert('Parabéns') // Fim de Jogo
+                $('.completed-game').toggle()
             }
             break
 
         case 'medium':
             if (allTurned.length >= 12) {
-                alert('Parabéns') // Fim de Jogo
+                $('.completed-game').toggle()
             }
             break
 
         case 'large':
             if (allTurned.length >= 24) {
-                alert('Parabéns') // Fim de Jogo
+                $('.completed-game').toggle()
             }
             break
     }
+}
+
+function routinePlayAgain() {
+    containerBoardPieces.innerHTML = ''
+    previousPiece = ''
+    nextPiece = ''
+    running = true
+    allTurned = []
+    preparedArray = []
+    shuffledData = []
+
+    $('.main-game').toggle()
+    $('.main-frame').toggle()
+    $('.completed-game').toggle()
 }
 
 function inicia() {
